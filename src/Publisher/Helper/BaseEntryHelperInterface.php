edf86adae0d7c7af44fcf98881ced15090493c52
Jacqueline Lueken <jac_lueken@live.de>
2016-08-20 01:22:15 +0200

    added docu

--- src/Publisher/Helper/BaseEntryHelperInterface.php
+++ src/Publisher/Helper/BaseEntryHelperInterface.php
@@ -5,12 +5,44 @@ namespace Publisher\Helper;
 interface BaseEntryHelperInterface
 {
     
+    /**
+     * Returns the service ID of $entryId.
+     * 
+     * Example:
+     * getServiceId('TwitterUser') returns 'Twitter'
+     * 
+     * @param string $entryId ID of an Entry
+     * 
+     * @return string
+     */
     public function getServiceId(string $entryId);
     
+    /**
+     * Returns the full class name of the Entry with the ID $entryId.
+     * 
+     * @param string $entryId ID of an Entry
+     * 
+     * @return string
+     */
     public function getEntryClass(string $entryId);
     
+    /**
+     * Returns the full class name of the Selector
+     * that belongs to an Entry with the ID $entryId.
+     * 
+     * @param string $entryId ID of an Entry
+     * 
+     * @return string
+     */
     public function getSelectorClass(string $entryId);
     
+    /**
+     * Returns the full class name of the Mode with the ID $modeId.
+     * 
+     * @param string $modeId ID of an Mode
+     * 
+     * @return string
+     */
     public function getModeClass(string $modeId);
     
 }

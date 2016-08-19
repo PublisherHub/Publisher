9c25bfdc450870ae7f800a4979fe95abb22b8730
Jacqueline Lueken <jac_lueken@live.de>
2016-08-20 01:24:21 +0200

    added missing interface

--- src/Publisher/Supervision/PublisherSupervisorInterface.php
+++ src/Publisher/Supervision/PublisherSupervisorInterface.php
@@ -20,4 +20,51 @@ interface PublisherSupervisorInterface
      */
     public function checkConfig();
     
+    /**
+     * Returns the ids of the configured services
+     * 
+     * @return array
+     */
+    public function getServices();
+    
+    /**
+     * Returns the configured subtypes of each service
+     * 
+     * Example: If we have following configuration
+     *  $config = array(
+     *      'entries' =>
+     *          'Facebook' => array('User', 'Page')
+     *  );
+     * getEntryIds('Facebook') will return
+     *  array('User', 'Page')
+     * 
+     * @return array
+     */
+    public function getEntrySubtypes();
+    
+    
+    /**
+     * Returns the IDs of all available Entries of $service.
+     * 
+     * Example: If we have following configuration
+     *  $config = array(
+     *      'entries' =>
+     *          'Facebook' => array('User', 'Page')
+     *  );
+     * getEntryIds('Facebook') will return
+     *  array('FacebookUser', 'FacebookPage')
+     * 
+     * @param string $serviceId
+     * @return array
+     */
+    public function getEntryIds(string $serviceId);
+    
+    /**
+     * Returns all configured modes.
+     * That doesn't mean that each entry supports this mode.
+     * 
+     * @return array
+     */
+    public function getAllModes();
+    
 }
